6f68f65335a02a1d1a20edea7d0407da9a9b4296
Josef Stark <j.stark@tum.de>
2017-09-26 23:11:07 +0200

    more compiling changes

--- dl_and_build_local_opencv_cuda_enabled.sh
+++ dl_and_build_local_opencv_cuda_enabled.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-THIRD_P_PATH="$(pwd)/third_party/include/"
+# we set this path to somewhere outside the project so that remote building doesn't transfer our whole
+# OpenCV build directoy. That path must be supplied to CMakeLists.txt so it can find the libraries.
+THIRD_P_PATH="${HOME}/cudalab-deps/"
 REL_BUILD_PATH="build/"
 REL_INST_PATH="local-install/"
 
@@ -10,6 +12,7 @@ DL_FILENAME_EXT="zip"
 DL_PATH="https://github.com/opencv/opencv/archive/"
 FOLDERNAME_UNPACKED="opencv-${DL_FILENAME}"
 
+mkdir -p ${THIRD_P_PATH}
 cd ${THIRD_P_PATH}
 
 ABS_INSTALL_PATH="$(pwd)/${FOLDERNAME_UNPACKED}/${REL_INST_PATH}"
@@ -30,7 +33,9 @@ rm "${DL_FILENAME}.${DL_FILENAME_EXT}"
 cd "${FOLDERNAME_UNPACKED}"
 mkdir $REL_BUILD_PATH
 cd $REL_BUILD_PATH
-cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=OFF -D CMAKE_INSTALL_PREFIX="${ABS_INSTALL_PATH}" \
+#cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=OFF -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0/ \
+cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0/ \
+-D CMAKE_INSTALL_PREFIX="${ABS_INSTALL_PATH}" \
 -DBUILD_opencv_core=ON -DBUILD_opencv_flann=ON -DBUILD_opencv_imgproc=ON -DBUILD_opencv_highgui=ON \
 -DBUILD_opencv_features2d=ON -DBUILD_opencv_calib3d=ON -DBUILD_opencv_ml=ON -DBUILD_opencv_video=ON \
 -DBUILD_opencv_legacy=ON -DBUILD_opencv_objdetect=ON -DBUILD_opencv_photo=ON -DBUILD_opencv_gpu=ON \
