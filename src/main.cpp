b907241a8d7981078c4515f55fbe134335a5d57d
gu32deb <christoph.kick@tum.de>
2017-10-02 14:29:01 +0200

    trajectory visualization

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,13 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
+#include <opencv2/viz/viz3d.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/core/affine.hpp>
 
 #include "dvo.hpp"
 #include "tum_benchmark.hpp"
+
 
 #define STR1(x)  #x
 #define STR(x)  STR1(x)
@@ -88,6 +91,19 @@ int main(int argc, char *argv[])
     // process frames
     double runtimeAvg = 0.0;
     int framesProcessed = 0;
+
+    // code for visualization
+    cv::viz::Viz3d mainWindow("Odometry");
+    mainWindow.setWindowSize(cv::Size(1600,900));
+    cv::Matx33f vizK(K(0,0),K(0,1),K(0,2),
+                    K(1,0),K(1,1),K(1,2),
+                    K(2,0),K(2,1),K(2,2));
+    std::cout << vizK << std::endl;
+
+    std::vector<cv::Affine3f> vizPoses;
+    vizPoses.push_back(cv::Affine3f());
+
+
     for (size_t i = 1; i < numFrames && (maxFrames < 0 || i < maxFrames); ++i)
     {
         std::cout << "aligning frames " << (i-1) << " and " << i  << std::endl;
@@ -121,9 +137,37 @@ int main(int argc, char *argv[])
         depthRefGPUPyramid = depthCurGPUPyramid;
         grayRefGPUPyramid = grayCurGPUPyramid;
         ++framesProcessed;
+
+        float data[] = {
+            absPose(0,0),absPose(0,1),absPose(0,2),absPose(0,3),
+            absPose(1,0),absPose(1,1),absPose(1,2),absPose(1,3),
+            absPose(2,0),absPose(2,1),absPose(2,2),absPose(2,3),
+            absPose(3,0),absPose(3,1),absPose(3,2),absPose(3,3)
+        };
+
+        // matPose;
+        //cv::eigen2cv(absPose, matPose);
+        vizPoses.push_back(cv::Affine3f(data));
+
+        /*mainWindow.showWidget("camera"+i,cv::viz::WCameraPosition(vizK,0.02),vizPoses[i]);
+
+        cv::Vec3f start = vizPoses[i-1].translation();
+        cv::Vec3f end = vizPoses[i].translation();
+
+        mainWindow.showWidget("line"+i,cv::viz::WLine(cv::Point3f(start),cv::Point3f(end),cv::viz::Color::green()));
+        */
+        mainWindow.showWidget("cameras_line",cv::viz::WTrajectory(vizPoses, cv::viz::WTrajectory::PATH, 0.05, cv::viz::Color::green()));
+        mainWindow.showWidget("cameras_frustums", cv::viz::WTrajectoryFrustums(vizPoses, vizK, 0.05, cv::viz::Color::red()));
+        mainWindow.spinOnce(30);
+
+
+
+
+
     }
     std::cout << "average runtime: " << (runtimeAvg / framesProcessed) * 1000.0 << " ms" << std::endl;
 
+    mainWindow.spin();
     // save poses
     savePoses(dataFolder + "traj.txt", poses, timestamps);
 
@@ -131,5 +175,6 @@ int main(int argc, char *argv[])
     cv::destroyAllWindows();
     cublasDestroy(handle);
     std::cout << "Direct Image Alignment finished." << std::endl;
+
     return 0;
 }
