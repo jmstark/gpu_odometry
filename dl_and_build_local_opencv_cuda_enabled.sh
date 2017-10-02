193795e45318f11c5f882710033d59f6d2673862
Josef Stark <j.stark@tum.de>
2017-10-02 19:17:39 +0200

    OpenCV 3: Update dl_and_build_local_opencv_cuda_enabled.sh

--- dl_and_build_local_opencv_cuda_enabled.sh
+++ dl_and_build_local_opencv_cuda_enabled.sh
@@ -6,8 +6,8 @@ THIRD_P_PATH="${HOME}/cudalab-deps/"
 REL_BUILD_PATH="build/"
 REL_INST_PATH="local-install/"
 
-#https://github.com/opencv/opencv/archive/2.4.13.3.zip
-DL_FILENAME="2.4.13.3"
+#https://github.com/opencv/opencv/archive/3.3.0.zip
+DL_FILENAME="3.3.0"
 DL_FILENAME_EXT="zip"
 DL_PATH="https://github.com/opencv/opencv/archive/"
 FOLDERNAME_UNPACKED="opencv-${DL_FILENAME}"
@@ -25,23 +25,32 @@ else
 	echo "Could not find local OpenCV install ${ABS_INSTALL_PATH}; commencing build."
 fi
 
-rm -f "${DL_FILENAME}.${DL_FILENAME_EXT}"
-rm -rf "${FOLDERNAME_UNPACKED}"
-wget "${DL_PATH}${DL_FILENAME}.${DL_FILENAME_EXT}"
-unzip -q "${DL_FILENAME}.${DL_FILENAME_EXT}"
-rm "${DL_FILENAME}.${DL_FILENAME_EXT}"
+#rm -f "${DL_FILENAME}.${DL_FILENAME_EXT}"
+#rm -rf "${FOLDERNAME_UNPACKED}"
+#wget "${DL_PATH}${DL_FILENAME}.${DL_FILENAME_EXT}"
+#unzip -q "${DL_FILENAME}.${DL_FILENAME_EXT}"
+#rm "${DL_FILENAME}.${DL_FILENAME_EXT}"
 cd "${FOLDERNAME_UNPACKED}"
 mkdir $REL_BUILD_PATH
 cd $REL_BUILD_PATH
-#cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=OFF -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0/ \
-cmake -D CMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=ON -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0/ \
--D CMAKE_INSTALL_PREFIX="${ABS_INSTALL_PATH}" \
--DBUILD_opencv_core=ON -DBUILD_opencv_flann=ON -DBUILD_opencv_imgproc=ON -DBUILD_opencv_highgui=ON \
--DBUILD_opencv_features2d=ON -DBUILD_opencv_calib3d=ON -DBUILD_opencv_ml=ON -DBUILD_opencv_video=ON \
--DBUILD_opencv_legacy=ON -DBUILD_opencv_objdetect=ON -DBUILD_opencv_photo=ON -DBUILD_opencv_gpu=ON \
--DBUILD_opencv_ocl=OFF -DBUILD_opencv_nonfree=OFF -DBUILD_opencv_contrib=OFF -DBUILD_opencv_java=OFF \
--DBUILD_opencv_python=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_superres=OFF -DBUILD_opencv_ts=OFF \
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0/ \
+-DCMAKE_INSTALL_PREFIX="${ABS_INSTALL_PATH}" \
+-DWITH_CUDA=ON -DWITH_CUDALEGACY=ON -DBUILD_opencv_java=OFF \
+-DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF \
+-DBUILD_opencv_stitching=OFF -DBUILD_opencv_superres=OFF -DBUILD_opencv_ts=OFF \
 -DBUILD_opencv_videostab=OFF \
+-DWITH_VTK=ON -DWITH_OPENNI2=ON \
 ..
-make -j
+make -j2
 make install
+
+
+# -DOPENNI_PRIME_SENSOR_MODULE_BIN_DIR=/usr/prakt/s164/cudalab-deps/OpenNI2/Bin/x64-Release/OpenNI2/Drivers \
+#-DOPENNI2_LIB_DIR=/home/josef/git/cudalab/third_party/OpenNI2/Bin/x64-Release/ \
+#-DOPENNI2_INCLUDE_DIR=/home/josef/git/cudalab/third_party/OpenNI2/Include/ \
+#-DOPENNI2_INSTALL_PATH=/home/josef/git/cudalab/third_party/OpenNI2/ \
+
+
+#-DBUILD_opencv_core=ON -DBUILD_opencv_flann=ON -DBUILD_opencv_imgproc=ON -DBUILD_opencv_highgui=ON \
+#-DBUILD_opencv_features2d=ON -DBUILD_opencv_calib3d=ON -DBUILD_opencv_ml=ON -DBUILD_opencv_video=ON \
+#-DBUILD_opencv_legacy=ON -DBUILD_opencv_objdetect=ON -DBUILD_opencv_photo=ON -DBUILD_opencv_gpu=ON \
