f9a8946f00ae11712298c5e1d4dafaf21c099d7e
Josef Stark <j.stark@tum.de>
2017-10-03 22:25:34 +0200

    Integrated kinect into main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <Eigen/Dense>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/videoio/videoio.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -24,59 +25,83 @@ cublasHandle_t handle;
 
 int main(int argc, char *argv[])
 {
-    std::string dataFolder = std::string(STR(DVO_SOURCE_DIR)) + "/data/";
-
-    cublasCreate(&handle);
-
-    Eigen::Matrix3f K;
-#if 1
-    // initialize intrinsic matrix: fr1
-    K <<    517.3, 0.0, 318.6,
-            0.0, 516.5, 255.3,
-            0.0, 0.0, 1.0;
-    if(argc<2)
-    {
-      std::cerr<<"Usage: ./dvo <path to data folder>\n";
-      return 1;
-    }
-    dataFolder = argv[1];
-    //dataFolder = "/work/maierr/rgbd_data/rgbd_dataset_freiburg1_desk2/";
-#else
-    dataFolder = "/work/maierr/rgbd_data/rgbd_dataset_freiburg3_long_office_household/";
-    // initialize intrinsic matrix: fr3
-    K <<    535.4, 0.0, 320.1,
-            0.0, 539.2, 247.6,
-            0.0, 0.0, 1.0;
-#endif
-    //std::cout << "Camera matrix: " << K << std::endl;
-
-    // load file names
-    std::string assocFile = dataFolder + "rgbd_assoc.txt";
-    std::cout << assocFile << std::endl;
+    cv::VideoCapture* capture;
+    bool useKinect = false;
+    int maxFrames = -1;
+    int numFrames;
+    std::string assocFile;
     std::vector<std::string> filesColor;
     std::vector<std::string> filesDepth;
     std::vector<double> timestampsDepth;
     std::vector<double> timestampsColor;
-    if (!loadAssoc(assocFile, filesDepth, filesColor, timestampsDepth, timestampsColor))
+    std::vector<double> timestamps;
+    cv::Mat grayRef;
+    cv::Mat depthRef;  
+    int w, h;
+    std::string dataFolder = std::string(STR(DVO_SOURCE_DIR)) + "/data/";
+    cublasCreate(&handle);
+    Eigen::Matrix3f K;
+
+    // initialize intrinsic matrix: fr1
+    K <<    517.3, 0.0, 318.6,
+            0.0, 516.5, 255.3,
+            0.0, 0.0, 1.0;
+    //Code for initializing kinect stuff
+    if(argc<2)
     {
-        std::cout << "Assoc file could not be loaded!" << std::endl;
-        return 1;
+        useKinect = true;
+        // CAP_OPENNI2_ASUS driver does not support grabbing bgr-images (Image generator), while this one does
+        capture = new cv::VideoCapture( cv::CAP_OPENNI2 );
+       	if( !capture->isOpened() )
+        {
+            std::cout << "***** Can not open a capture object." << std::endl;
+            return -1;
+        }
+        // Print some avalible device settings.
+        if (capture->get(cv::CAP_OPENNI_DEPTH_GENERATOR_PRESENT))
+        {
+            std::cout << "\nDepth generator output mode:" << std::endl <<
+                "FRAME_WIDTH      " << capture->get(cv::CAP_PROP_FRAME_WIDTH) << std::endl <<
+                "FRAME_HEIGHT     " << capture->get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl <<
+                "FRAME_MAX_DEPTH  " << capture->get(cv::CAP_PROP_OPENNI_FRAME_MAX_DEPTH) << " mm" << std::endl <<
+                "FPS              " << capture->get(cv::CAP_PROP_FPS) << std::endl <<
+                "REGISTRATION     " << capture->get(cv::CAP_PROP_OPENNI_REGISTRATION) << std::endl <<
+                "IMG_GEN          " << capture->get(cv::CAP_OPENNI_IMAGE_GENERATOR_PRESENT) << std::endl;
+        }
+        else
+        {
+            std::cout << "\nDevice doesn't contain depth generator or it is not selected." << std::endl;
+            return 1;
+        }
+        //get (first) reference images from camera
+        capture->grab();
+        capture->retrieve( depthRef, cv::CAP_OPENNI_DEPTH_MAP );
+        capture->retrieve( grayRef, cv::CAP_OPENNI_GRAY_IMAGE );
+        timestamps.push_back((double)cv::getTickCount()/cv::getTickFrequency());
     }
-    int numFrames = filesDepth.size();
-
-    int maxFrames = -1;
-    maxFrames = 100;
-
+    //Code for initializing stuff for reading images from files
+    else
+    {
+        dataFolder = std::string(argv[1]) + "/";
+        // load file names
+        assocFile = dataFolder + "rgbd_assoc.txt";
+        if (!loadAssoc(assocFile, filesDepth, filesColor, timestampsDepth, timestampsColor))
+        {
+            std::cout << "Assoc file could not be loaded!" << std::endl;
+            return 1;
+        }
+        numFrames = filesDepth.size();
+        maxFrames = 100;
+        timestamps.push_back(timestampsDepth[0]);
+        grayRef = loadGray(dataFolder + filesColor[0]);
+        depthRef = loadDepth(dataFolder + filesDepth[0]);
+    }
+    
+    w = depthRef.cols;
+    h = depthRef.rows;    
     Eigen::Matrix4f absPose = Eigen::Matrix4f::Identity();
     std::vector<Eigen::Matrix4f> poses;
-    std::vector<double> timestamps;
     poses.push_back(absPose);
-    timestamps.push_back(timestampsDepth[0]);
-
-    cv::Mat grayRef = loadGray(dataFolder + filesColor[0]);
-    cv::Mat depthRef = loadDepth(dataFolder + filesDepth[0]);
-    int w = depthRef.cols;
-    int h = depthRef.rows;
 
     DVO dvo;
     dvo.init(w, h, K);
@@ -91,14 +116,28 @@ int main(int argc, char *argv[])
     for (size_t i = 1; i < numFrames && (maxFrames < 0 || i < maxFrames); ++i)
     {
         std::cout << "aligning frames " << (i-1) << " and " << i  << std::endl;
+        double timeDepth1;
+        cv::Mat depthCur;
+        cv::Mat grayCur;
 
+        if(useKinect)
+        {
+            //get images from camera
+            capture->grab();
+            capture->retrieve( depthCur, cv::CAP_OPENNI_DEPTH_MAP );
+            capture->retrieve( grayCur, cv::CAP_OPENNI_GRAY_IMAGE );  
+            timeDepth1 = (double)cv::getTickCount()/cv::getTickFrequency();
+        }
+        else
+        {
         // load input frame
         std::string fileColor1 = filesColor[i];
         std::string fileDepth1 = filesDepth[i];
-        double timeDepth1 = timestampsDepth[i];
+        timeDepth1 = timestampsDepth[i];
         //std::cout << "File " << i << ": " << fileColor1 << ", " << fileDepth1 << std::endl;
-        cv::Mat grayCur = loadGray(dataFolder + fileColor1);
-        cv::Mat depthCur = loadDepth(dataFolder + fileDepth1);
+        grayCur = loadGray(dataFolder + fileColor1);
+        depthCur = loadDepth(dataFolder + fileDepth1);
+        }
         // build pyramid
         std::vector<cv::cuda::GpuMat> grayCurGPUPyramid;
         std::vector<cv::cuda::GpuMat> depthCurGPUPyramid;
@@ -130,6 +169,7 @@ int main(int argc, char *argv[])
     // clean up
     cv::destroyAllWindows();
     cublasDestroy(handle);
+    delete(capture);
     std::cout << "Direct Image Alignment finished." << std::endl;
     return 0;
 }
